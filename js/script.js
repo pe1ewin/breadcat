9eb268f187e370d46095197b5e45fbf5774443fe
Pelewin <167339725+pe1ewin@users.noreply.github.com>
2024-07-26 14:27:15 +0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let showPop = document.getElementById("ShowForm")
+function showForm() {
+    showPop.style.display = "block";
+}
+function closeForm() {
+    showPop.style.display = "none";
+    alert("Успешно отправлено!")
+}
